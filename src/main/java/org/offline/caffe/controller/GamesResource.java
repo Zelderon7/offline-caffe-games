e3f4642ab804c91567e607dcbbe98aeaa425a8a1
Zelderon7 <zelderon038@gmail.com>
2025-09-19 09:42:12 +0300

    Created test endpoints in controllers

--- src/main/java/org/offline/caffe/controller/GamesResource.java
+++ src/main/java/org/offline/caffe/controller/GamesResource.java
@@ -1,2 +1,17 @@
-package org.offline.caffe.controller;public class GamesResource {
+package org.offline.caffe.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/games")
+public class GamesResource {
+
+    @GET
+    @Path("/count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int getGamesCount(){
+        return 0;
+    }
 }
--- src/main/java/org/offline/caffe/controller/HelloResource.java
+++ src/main/java/org/offline/caffe/controller/HelloResource.java
@@ -1,2 +1,16 @@
-package org.offline.caffe.controller;public class HelloResource {
+package org.offline.caffe.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/test")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World!";
+    }
 }
